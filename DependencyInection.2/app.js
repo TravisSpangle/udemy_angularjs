8006a6c236a518b0866ce42c1f45e14567b13fbd
Travis Spangle <travis.spangle@k2sports.com>
2019-03-03 14:09:54 -0800

    DI with minified code
    By providing any array we can tie our parameters to minified parameters.

---
+++ DependencyInection.2/app.js
@@ -0,0 +1,12 @@
+// MODULE
+var angularApp = angular.module('travisApp', []);
+
+// CONTROLLERS
+// angularApp.controller('mainController', ['$scope', '$log', function ($scope, $log) {
+// 
+//   $log.info($scope);
+// 
+// }]);
+
+// Minified
+angularApp.controller("mainController",["$scope","$log",function(o,n){n.info(o)}]);
