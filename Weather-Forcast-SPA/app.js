b923d63159cea7a9e301c833d3af80342aa57f43
Travis Spangle <travis.spangle@k2sports.com>
2019-04-07 18:33:06 -0700

    creating a custom service

--- Weather-Forcast-SPA/app.js
+++ Weather-Forcast-SPA/app.js
@@ -19,9 +19,22 @@ weatherApp.config(function ($routeProvider) {
 
 });
 
+// Services ________________________________________________________
+
+weatherApp.service('cityService', function(){
+  this.city = "New York, NY";
+});
+
 // Controllers _____________________________________________________
-weatherApp.controller('homeController', ['$scope', function($scope){
+weatherApp.controller('homeController', ['$scope', 'cityService',
+  function($scope, cityService){
+    $scope.city = cityService.city;
+    $scope.$watch('city', function() {
+      cityService.city = $scope.city;
+    });
 }]);
 
-weatherApp.controller('forcastController', ['$scope', function($scope){
+weatherApp.controller('forcastController', ['$scope', 'cityService', 
+  function($scope, cityService){
+    $scope.city = cityService.city;
 }]);
